a25cd9ab806bbe64209dab1b704519fffe52366f
Richard Horton <hortonr6@coventry.ac.uk>
2019-03-26 11:15:04 +0000

    Added DB Connection Test

--- src/membership/system/MembershipSystem.java
+++ src/membership/system/MembershipSystem.java
@@ -5,6 +5,8 @@
  */
 package membership.system;
 
+
+import java.sql.*;
 /**
  *
  * @author hortonr6
@@ -17,6 +19,46 @@ public class MembershipSystem {
     public static void main(String[] args) {
         Login mainApp = new Login();
         mainApp.setVisible(true);
+        
+        // DB Connection
+        String connectionURL = "jdbc:derby://localhost:1527/MembershipDB";
+        String uName = "user1";
+        String uPass = "password";
+        
+        try
+        {
+            Connection conn = DriverManager.getConnection(connectionURL, uName, uPass);
+            System.out.println("Connecting...");
+            
+            if(conn != null)
+            {
+                Statement st = conn.createStatement();
+                ResultSet rs = null;
+                
+                System.out.println("Sample Record:");
+                System.out.println("---------------------");
+                String sql = "SELECT * FROM Member";
+                rs=st.executeQuery(sql);
+                while(rs.next())
+                { 
+                    System.out.println(rs.getInt("MEMBERID")+"\t"+rs.getString("FIRSTNAME"));
+                }
+                
+                rs.close();
+                st.close();
+                conn.close();
+                System.out.println("Connection is closed."); 
+            }
+            else
+            {
+                System.out.println("null");  
+            }
+        }
+        catch(SQLException ex)
+        {             
+            System.out.println("SQLException error");
+            System.out.println(ex.getMessage());
+        }
     }
-    
 }
+
