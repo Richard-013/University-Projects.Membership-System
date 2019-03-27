c9b17460ffdb8ac74d3a34881d88e593f689f416
Richard Horton (hortonr6) <hortonr6@coventry.ac.uk>
2019-03-27 19:44:12 +0000

    Formatting

--- src/membership/system/DBProxyMembership.java
+++ src/membership/system/DBProxyMembership.java
@@ -6,66 +6,66 @@ import javax.swing.table.DefaultTableModel;
 
 public class DBProxyMembership
 {
-	public static void getMemberDetails()
-        {
-            // TODO - implement DBProxyMembership.getMemberDetails
-	}
+    public static void getMemberDetails()
+    {
+        // TODO - implement DBProxyMembership.getMemberDetails
+    }
 
-	/**
-	 * 
-	 * @param member
-	 */
-	public static void changeMemberDetails(Member member)
-        {
-            // TODO - implement DBProxyMembership.changeMemberDetails
-	}
+    /**
+     * 
+     * @param member
+     */
+    public static void changeMemberDetails(Member member)
+    {
+        // TODO - implement DBProxyMembership.changeMemberDetails
+    }
 
-	/**
-	 * 
-	 * @param firstName
-	 */
-	public static void searchByName(String firstName, String sql)
+    /**
+     * 
+     * @param firstName
+     */
+    public static void searchByName(String firstName, String sql)
+    {
+        try
         {
-            try
+            Connection conn = DatabaseAccess.makeConnection();
+            System.out.println("Connecting...");
+
+            if(conn != null)
             {
-                Connection conn = DatabaseAccess.makeConnection();
-                System.out.println("Connecting...");
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ps.setString(1, firstName);
+                ResultSet rs = null;
 
-                if(conn != null)
-                {
-                    PreparedStatement ps = conn.prepareStatement(sql);
-                    ps.setString(1, firstName);
-                    ResultSet rs = null;
+                DefaultTableModel model = (DefaultTableModel) AdvisorUI.findID.memberTable.getModel();
 
-                    DefaultTableModel model = (DefaultTableModel) AdvisorUI.findID.memberTable.getModel();
+                rs = ps.executeQuery();
+                while(rs.next())
+                { 
+                    Vector<String> newRow =  new Vector<String>();
+                    newRow.add(Integer.toString(rs.getInt("MEMBERID")));
+                    newRow.add(rs.getString("FIRSTNAME"));
+                    newRow.add(rs.getString("LASTNAME"));
+                    newRow.add(rs.getString("ADDRESSLINE1"));
+                    newRow.add(rs.getString("POSTCODE"));
 
-                    rs = ps.executeQuery();
-                    while(rs.next())
-                    { 
-                        Vector<String> newRow =  new Vector<String>();
-                        newRow.add(Integer.toString(rs.getInt("MEMBERID")));
-                        newRow.add(rs.getString("FIRSTNAME"));
-                        newRow.add(rs.getString("LASTNAME"));
-                        newRow.add(rs.getString("ADDRESSLINE1"));
-                        newRow.add(rs.getString("POSTCODE"));
-
-                        model.addRow(newRow);
-                    }
-
-                    rs.close();
-                    ps.close();
-                    conn.close();
-                    System.out.println("Connection is closed.");  
+                    model.addRow(newRow);
                 }
-                else
-                {
-                    System.out.println("null");
-                }
+
+                rs.close();
+                ps.close();
+                conn.close();
+                System.out.println("Connection is closed.");  
             }
-            catch(SQLException ex)
-            {             
-                System.out.println("SQLException error");
-                System.out.println(ex.getMessage());
+            else
+            {
+                System.out.println("null");
             }
-	}
+        }
+        catch(SQLException ex)
+        {             
+            System.out.println("SQLException error");
+            System.out.println(ex.getMessage());
+        }
+    }
 }
