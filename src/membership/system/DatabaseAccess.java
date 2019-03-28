0f6745f49b00f764b8380ad0acfd1947de632580
Richard Horton <hortonr6@coventry.ac.uk>
2019-03-28 10:08:06 +0000

    Updated database connection

--- src/membership/system/DatabaseAccess.java
+++ src/membership/system/DatabaseAccess.java
@@ -5,9 +5,35 @@ import java.sql.*;
 public class DatabaseAccess
 {
     private static Connection conn;
+    private static final String CONNECTIONURL = "jdbc:derby://localhost:1527/MembershipDB";
+    private static final String UNAME = "user1";
+    private static final String UPASS = "password";
     
+    // DB Connection
     public static Connection makeConnection()
     {
+        conn = null;
+
+        try
+        {
+            conn = DriverManager.getConnection(CONNECTIONURL, UNAME, UPASS);
+            System.out.println("Connecting...");
+
+            if(conn != null)
+            {
+                return conn;
+            }
+            else
+            {
+                System.out.println("Could not connect to the database");  
+            }
+        }
+        catch(SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
         return conn;
     }
+    
 }
